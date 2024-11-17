bbfffa66f52ac0a1a23343d65c2501c26c8998c8
avraam311 <yandaroviba@mail.ru>
2024-11-17 21:42:44 +0300

    add slug url wrap

--- goods/views.py
+++ goods/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(1)
 
     context = {
         'title': 'Home - Каталог',
-        'goods': goods,
+        'goods': current_page,
     }
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'goods/product.html', context)
